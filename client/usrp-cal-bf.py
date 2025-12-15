caf44500a1e726607ec1b6791ca017718e97859f
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2025-12-15 15:22:28 +0100

    Update usrp-cal-bf.py

--- client/usrp-cal-bf.py
+++ client/usrp-cal-bf.py
@@ -1024,7 +1024,7 @@ def main():
                     phi_cable = phases_dict[HOSTNAME]
                     logger.debug(f"Applying phase correction: {phi_cable}")
                 else:
-                    logger.error("Phase offset not found in phase-reference-calibration.yml")
+                    logger.error("Phase offset not found in ref-RF-cable.yml")
             except yaml.YAMLError as exc:
                 print(exc)
 
@@ -1039,7 +1039,7 @@ def main():
                     phi_cable = phases_dict[HOSTNAME]
                     logger.debug(f"Applying phase correction: {phi_offset}")
                 else:
-                    logger.error("Phase offset not found in config-phase-offsets.yml")
+                    logger.error("Phase offset not found in tx-phases-benchmark.yml")
             except yaml.YAMLError as exc:
                 print(exc)
 
