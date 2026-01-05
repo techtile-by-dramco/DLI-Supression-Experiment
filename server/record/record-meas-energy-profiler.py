0017681b56152137c8c998c8eb7342567358be40
Gilles Callebaut <callebaut.gilles@gmail.com>
2026-01-05 09:53:02 +0100

    update exp

--- server/record/record-meas-energy-profiler.py
+++ server/record/record-meas-energy-profiler.py
@@ -17,7 +17,7 @@ import sys
 # ****************************************************************************************** #
 
 SAVE_EVERY = 60.0  # seconds
-FOLDER = "sionna0-2-A"  # subfolder inside data/ where to save measurement data
+FOLDER = "sionna1-2-A"  # subfolder inside data/ where to save measurement data
 TIMESTAMP = round(time())
 
 # -------------------------------------------------
